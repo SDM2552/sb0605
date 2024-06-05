68ce910fb7368cbd570f1552e15836be325600db
SDM2552 <se299792458@gmail.com>
2024-06-05 09:26:55 +0900

    init

---
+++ src/main/java/com/sky/sb0605/Sb0605Application.java
@@ -0,0 +1,13 @@
+package com.sky.sb0605;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb0605Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb0605Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/sb0605/Sb0605ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.sb0605;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb0605ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
